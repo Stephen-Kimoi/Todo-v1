5c173fda6fbfe9f749bbc4d539e1a6936035992c
Steve Kimoi <moenisteve@gmail.com>
2022-05-28 13:13:41 +0300

    coming up with the dummy app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, request 
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate 
+from flask import Flask, render_template
 
-app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URL'] = 'postgresql://postgres:abc@localhost:5432/todo-app'
-db = SQLAlchemy(app)
+app = Flask(__name__)
 
-migrate = Migrate(app, db)
+@app.route('/')
+def index():
+  return render_template('index.html', data=[{
+    'description': 'Todo 1'
+  }, {
+    'description': 'Todo 2'
+  }, {
+    'description': 'Todo 3'
+  }])
+
+
+
